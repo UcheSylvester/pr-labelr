db4d1bae514889b14c9b345fba18c75135adc405
UcheSylvester <okorocode@gmail.com>
2023-11-05 00:24:10 +0100

    chores: workflow for adding labels to PRs

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello world!!!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+const run = async () => {
+    const token = (0, core_1.getInput)("github-token");
+    const label = (0, core_1.getInput)("label");
+    const octokit = (0, github_1.getOctokit)(token);
+    const pullRequest = github_1.context.payload.pull_request;
+    console.log({ token, label, octokit, pullRequest });
+    try {
+        if (!pullRequest)
+            throw new Error("No pull request found");
+        const response = await octokit.rest.issues.addLabels({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+        console.log("added label successfully", { response });
+    }
+    catch (error) {
+        (0, core_1.setFailed)((error === null || error === void 0 ? void 0 : error.message) || "An unexpected error occurred!");
+    }
+};
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello world!!!");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+const run = async () => {
+  const token = getInput("github-token");
+  const label = getInput("label");
+
+  const octokit = getOctokit(token);
+
+  const pullRequest = context.payload.pull_request;
+  console.log({ token, label, octokit, pullRequest });
+
+  try {
+    if (!pullRequest) throw new Error("No pull request found");
+
+    const response = await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+    console.log("added label successfully", { response });
+  } catch (error) {
+    setFailed((error as Error)?.message || "An unexpected error occurred!");
+  }
+};
+
+run();
