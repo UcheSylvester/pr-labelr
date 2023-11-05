314231cb180784560ccd1d59381ae39491f60314
UcheSylvester <okorocode@gmail.com>
2023-11-05 04:17:27 +0100

    chores: add and use default label

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -23,10 +23,11 @@ describe('run', () => {
     expect(mockSetFailed).toHaveBeenCalledWith('No pull request found');
   });
 
-  it('should add a label to the pull request', async () => {
+  it('should add a label to the pull request based on the title', async () => {
     mockContext.payload = {
       pull_request: {
         number: 123,
+        title: '[HOTFIX]: This is a hotfix',
       },
     };
     (mockContext.repo as any) = {
@@ -35,7 +36,6 @@ describe('run', () => {
     };
 
     mockGetInput.mockReturnValueOnce('token');
-    mockGetInput.mockReturnValueOnce('label');
 
     const mockRest = {
       rest: {
@@ -50,20 +50,20 @@ describe('run', () => {
     await run();
 
     expect(mockGetInput).toHaveBeenCalledWith('github-token');
-    expect(mockGetInput).toHaveBeenCalledWith('label');
     expect(mockGetOctokit).toHaveBeenCalledWith('token');
     expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
       owner: 'owner',
       repo: 'repo',
       issue_number: 123,
-      labels: ['label'],
+      labels: ['hotfix'],
     });
   });
 
-  it('should set failed if an error occurs', async () => {
+  it('should add a label to the pull request based on the title (no colon)', async () => {
     mockContext.payload = {
       pull_request: {
         number: 123,
+        title: '[HOTFIX] This is a hotfix',
       },
     };
     (mockContext.repo as any) = {
@@ -72,7 +72,80 @@ describe('run', () => {
     };
 
     mockGetInput.mockReturnValueOnce('token');
-    mockGetInput.mockReturnValueOnce('label');
+
+    const mockRest = {
+      rest: {
+        issues: {
+          addLabels: jest.fn(),
+        },
+      },
+    };
+
+    mockGetOctokit.mockReturnValueOnce(mockRest as any);
+
+    await run();
+
+    expect(mockGetInput).toHaveBeenCalledWith('github-token');
+    expect(mockGetOctokit).toHaveBeenCalledWith('token');
+    expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      issue_number: 123,
+      labels: ['hotfix'],
+    });
+  });
+
+  it('should add default label to the pull request if no label is provided in title', async () => {
+    mockContext.payload = {
+      pull_request: {
+        number: 123,
+        title: 'This is a hotfix',
+      },
+    };
+    (mockContext.repo as any) = {
+      owner: 'owner',
+      repo: 'repo',
+    };
+
+    mockGetInput.mockReturnValueOnce('token');
+    mockGetInput.mockReturnValueOnce('pending-review');
+
+    const mockRest = {
+      rest: {
+        issues: {
+          addLabels: jest.fn(),
+        },
+      },
+    };
+
+    mockGetOctokit.mockReturnValueOnce(mockRest as any);
+
+    await run();
+
+    expect(mockGetInput).toHaveBeenCalledWith('github-token');
+    expect(mockGetInput).toHaveBeenCalledWith('default-label');
+    expect(mockGetOctokit).toHaveBeenCalledWith('token');
+    expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      issue_number: 123,
+      labels: ['pending-review'],
+    });
+  });
+
+  it('should set failed if an error occurs', async () => {
+    mockContext.payload = {
+      pull_request: {
+        number: 123,
+        title: '[HOTFIX]: This is a hotfix',
+      },
+    };
+    (mockContext.repo as any) = {
+      owner: 'owner',
+      repo: 'repo',
+    };
+
+    mockGetInput.mockReturnValueOnce('token');
 
     const mockRest = {
       rest: {
@@ -87,14 +160,50 @@ describe('run', () => {
     await run();
 
     expect(mockGetInput).toHaveBeenCalledWith('github-token');
-    expect(mockGetInput).toHaveBeenCalledWith('label');
     expect(mockGetOctokit).toHaveBeenCalledWith('token');
     expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
       owner: 'owner',
       repo: 'repo',
       issue_number: 123,
-      labels: ['label'],
+      labels: ['hotfix'],
     });
     expect(mockSetFailed).toHaveBeenCalledWith('test error');
+  });
+
+  it('should set failed with `An unexpected error occurred!` error message if an error occurs without an error message', async () => {
+    mockContext.payload = {
+      pull_request: {
+        number: 123,
+        title: '[HOTFIX]: This is a hotfix',
+      },
+    };
+    (mockContext.repo as any) = {
+      owner: 'owner',
+      repo: 'repo',
+    };
+
+    mockGetInput.mockReturnValueOnce('token');
+
+    const mockRest = {
+      rest: {
+        issues: {
+          addLabels: jest.fn().mockRejectedValueOnce(new Error()),
+        },
+      },
+    };
+
+    mockGetOctokit.mockReturnValueOnce(mockRest as any);
+
+    await run();
+
+    expect(mockGetInput).toHaveBeenCalledWith('github-token');
+    expect(mockGetOctokit).toHaveBeenCalledWith('token');
+    expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      issue_number: 123,
+      labels: ['hotfix'],
+    });
+    expect(mockSetFailed).toHaveBeenCalledWith('An unexpected error occurred!');
   });
 });
