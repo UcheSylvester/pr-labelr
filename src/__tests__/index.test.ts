56084207e21cffa9239e7f5334ceba555c4bd96b
UcheSylvester <okorocode@gmail.com>
2023-11-05 01:40:15 +0100

    chores: add tests

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,100 @@
+import { run } from "../index";
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+jest.mock("@actions/core");
+jest.mock("@actions/github");
+
+describe("run", () => {
+  const mockGetInput = getInput as jest.MockedFunction<typeof getInput>;
+  const mockSetFailed = setFailed as jest.MockedFunction<typeof setFailed>;
+  const mockGetOctokit = getOctokit as jest.MockedFunction<typeof getOctokit>;
+  const mockContext = context as jest.Mocked<typeof context>;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should throw an error if no pull request is found", async () => {
+    mockContext.payload = {};
+
+    await run();
+
+    expect(mockSetFailed).toHaveBeenCalledWith("No pull request found");
+  });
+
+  it("should add a label to the pull request", async () => {
+    mockContext.payload = {
+      pull_request: {
+        number: 123,
+      },
+    };
+    (mockContext.repo as any) = {
+      owner: "owner",
+      repo: "repo",
+    };
+
+    mockGetInput.mockReturnValueOnce("token");
+    mockGetInput.mockReturnValueOnce("label");
+
+    const mockRest = {
+      rest: {
+        issues: {
+          addLabels: jest.fn(),
+        },
+      },
+    };
+
+    mockGetOctokit.mockReturnValueOnce(mockRest as any);
+
+    await run();
+
+    expect(mockGetInput).toHaveBeenCalledWith("github-token");
+    expect(mockGetInput).toHaveBeenCalledWith("label");
+    expect(mockGetOctokit).toHaveBeenCalledWith("token");
+    expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: "owner",
+      repo: "repo",
+      issue_number: 123,
+      labels: ["label"],
+    });
+  });
+
+  it("should set failed if an error occurs", async () => {
+    mockContext.payload = {
+      pull_request: {
+        number: 123,
+      },
+    };
+    (mockContext.repo as any) = {
+      owner: "owner",
+      repo: "repo",
+    };
+
+    mockGetInput.mockReturnValueOnce("token");
+    mockGetInput.mockReturnValueOnce("label");
+
+    const mockRest = {
+      rest: {
+        issues: {
+          addLabels: jest.fn().mockRejectedValueOnce(new Error("test error")),
+        },
+      },
+    };
+
+    mockGetOctokit.mockReturnValueOnce(mockRest as any);
+
+    await run();
+
+    expect(mockGetInput).toHaveBeenCalledWith("github-token");
+    expect(mockGetInput).toHaveBeenCalledWith("label");
+    expect(mockGetOctokit).toHaveBeenCalledWith("token");
+    expect(mockRest.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: "owner",
+      repo: "repo",
+      issue_number: 123,
+      labels: ["label"],
+    });
+    expect(mockSetFailed).toHaveBeenCalledWith("test error");
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import { getInput, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 
-const run = async () => {
+export const run = async () => {
   const token = getInput("github-token");
   const label = getInput("label");
 
   const octokit = getOctokit(token);
 
   const pullRequest = context.payload.pull_request;
-  console.log({ token, label, pullRequest });
 
   try {
     if (!pullRequest) throw new Error("No pull request found");
 
-    const response = await octokit.rest.issues.addLabels({
+    console.log("hello2");
+
+    await octokit.rest.issues.addLabels({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: pullRequest.number,
       labels: [label],
     });
-    console.log("added label successfully", { response });
   } catch (error) {
     setFailed((error as Error)?.message || "An unexpected error occurred!");
   }
